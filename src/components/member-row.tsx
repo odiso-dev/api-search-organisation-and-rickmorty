2de7b2629f536fb4cd5c2cccda577418c24a3433
Oscar <trabajos1379@gmail.com>
2025-03-20 20:35:34 +0100

    integrado MUI

--- src/components/member-row.tsx
+++ src/components/member-row.tsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { MemberVm } from '@/member-row.vm';
 import { Image } from './image';
 import classes from './member-row.module.css';
+/* MUI */
+import { TableRow, TableCell } from '@mui/material/';
 
 interface Props {
   member: MemberVm;
@@ -11,20 +13,20 @@ interface Props {
 export const Member: React.FC<Props> = (props) => {
   const { member } = props;
   return (
-    <tr>
-      <td>
+    <TableRow>
+      <TableCell>
         <Image
           src={member.avatar_url}
           alt={member.login}
           classname={classes.member_img}
         />
-      </td>
-      <td>
+      </TableCell>
+      <TableCell sx={{ fontSize: '16px' }}>
         <p>{member.id}</p>
-      </td>
-      <td>
+      </TableCell>
+      <TableCell sx={{ fontSize: '16px' }}>
         <Link to={`/detail/${member.login}`}>{member.login}</Link>
-      </td>
-    </tr>
+      </TableCell>
+    </TableRow>
   );
 };
--- src/components/member-table.tsx
+++ src/components/member-table.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import classes from './member-table.module.css';
 import { Member } from './member-row';
 import { MemberVm } from '../member-row.vm';
+/* MUI */
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableContainer,
+  Paper,
+} from '@mui/material/';
 
 interface Props {
   members: MemberVm[];
@@ -10,19 +20,43 @@ interface Props {
 export const MemberTable: React.FC<Props> = (props) => {
   const { members } = props;
   return (
-    <table>
-      <thead>
-        <tr>
-          <th className={classes.listHeader}>Avatar</th>
-          <th className={classes.listHeader}>Id</th>
-          <th className={classes.listHeader}>Name</th>
-        </tr>
-      </thead>
-      <tbody>
-        {members.map((member) => (
-          <Member key={member.id} member={member} />
-        ))}
-      </tbody>
-    </table>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow
+            sx={{
+              '& th': { color: '#fff', fontWeight: 'bold', fontSize: '18px' },
+            }}
+          >
+            <TableCell
+              component="th"
+              scope="row"
+              className={classes.listHeader}
+            >
+              Avatar
+            </TableCell>
+            <TableCell
+              component="th"
+              scope="row"
+              className={classes.listHeader}
+            >
+              Id
+            </TableCell>
+            <TableCell
+              component="th"
+              scope="row"
+              className={classes.listHeader}
+            >
+              Name
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {members.map((member) => (
+            <Member key={member.id} member={member} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
